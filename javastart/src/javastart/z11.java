cc00893a3c5ca9416fab9d9085158f72c4ec3b4f
user <user@210.101.242.65>
2017-08-21 17:59:18 +0900

    11 수정

--- javastart/src/javastart/z11.java
+++ javastart/src/javastart/z11.java
@@ -1,7 +1,20 @@
 package javastart;
 
+class ax{
+	static int a=0;
+	String b;
+	ax(){
+		this("없음" + ++a);
+	}
+	ax(String n){
+		this.b=n;
+		System.out.println(this.b);
+	}
+}
 public class z11 {
 	public static void main(String args[]) {
-		System.out.println();
+		ax a=new ax();
+		ax a1=new ax("자바.txt");
+		ax a2=new ax();
 	}
 }
